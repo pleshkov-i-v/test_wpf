f450631626fed5531b1cabfcd5801de3ca606833
ivan_pleshkov <pleshkov.i.v@gmail.com>
2023-01-28 10:57:35 +0300

    Add database and input data

---
+++ TestWPF/TestWPF/Data/import_data.sql
@@ -0,0 +1,193 @@
+﻿-- Script Date: 1/28/2023 10:49 AM  - ErikEJ.SqlCeScripting version 3.5.2.94
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B01','FAM','39','Unkn','zd1',16.48,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B02','FAM','39','Unkn','zd2',16.27,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B03','FAM','39','Unkn','zd3',18.11,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B04','FAM','39','Unkn','zd4',16.12,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B05','FAM','39','Unkn','zd5',16.19,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B06','FAM','39','Unkn','zd6',15.87,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B07','FAM','39','Unkn','HSIL1',16.76,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B08','FAM','39','Unkn','HSIL2',17.36,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B09','FAM','39','Unkn','HSIL3',17.26,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B10','FAM','39','Unkn','HSIL4',15.80,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B11','FAM','39','Unkn','HSIL5',16.17,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('B12','FAM','39','Unkn','HSIL6',16.11,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C01','FAM','21','Unkn','zd1',25.05,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C02','FAM','21','Unkn','zd2',23.45,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C03','FAM','21','Unkn','zd3',30.73,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C04','FAM','21','Unkn','zd4',26.13,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C05','FAM','21','Unkn','zd5',25.99,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C06','FAM','21','Unkn','zd6',24.45,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C07','FAM','21','Unkn','HSIL1',27.42,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C08','FAM','21','Unkn','HSIL2',28.25,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C09','FAM','21','Unkn','HSIL3',26.98,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C10','FAM','21','Unkn','HSIL4',26.90,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C11','FAM','21','Unkn','HSIL5',31.86,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('C12','FAM','21','Unkn','HSIL6',25.08,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D01','FAM','29','Unkn','zd1',31.02,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D02','FAM','29','Unkn','zd2',34.23,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D03','FAM','29','Unkn','zd3',34.51,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D04','FAM','29','Unkn','zd4',27.52,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D05','FAM','29','Unkn','zd5',27.77,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D06','FAM','29','Unkn','zd6',31.42,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D07','FAM','29','Unkn','HSIL1',29.11,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D08','FAM','29','Unkn','HSIL2',28.75,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D09','FAM','29','Unkn','HSIL3',29.46,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D10','FAM','29','Unkn','HSIL4',29.59,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D11','FAM','29','Unkn','HSIL5',32.10,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('D12','FAM','29','Unkn','HSIL6',26.15,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E01','FAM','145','Unkn','zd1',29.53,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E02','FAM','145','Unkn','zd2',28.62,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E03','FAM','145','Unkn','zd3',29.79,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E04','FAM','145','Unkn','zd4',30.18,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E05','FAM','145','Unkn','zd5',31.64,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E06','FAM','145','Unkn','zd6',27.90,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E07','FAM','145','Unkn','HSIL1',23.03,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E08','FAM','145','Unkn','HSIL2',24.18,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E09','FAM','145','Unkn','HSIL3',23.54,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E10','FAM','145','Unkn','HSIL4',23.62,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E11','FAM','145','Unkn','HSIL5',29.29,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('E12','FAM','145','Unkn','HSIL6',24.79,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F01','FAM','451','Unkn','zd1',26.28,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F02','FAM','451','Unkn','zd2',32.46,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F03','FAM','451','Unkn','zd3',34.24,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F04','FAM','451','Unkn','zd4',22.14,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F05','FAM','451','Unkn','zd5',21.12,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F06','FAM','451','Unkn','zd6',32.27,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F07','FAM','451','Unkn','HSIL1',31.18,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F08','FAM','451','Unkn','HSIL2',30.87,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F09','FAM','451','Unkn','HSIL3',30.69,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F10','FAM','451','Unkn','HSIL4',29.08,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F11','FAM','451','Unkn','HSIL5',32.71,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('F12','FAM','451','Unkn','HSIL6',28.46,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G01','FAM','1246','Unkn','zd1',19.57,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G02','FAM','1246','Unkn','zd2',15.54,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G03','FAM','1246','Unkn','zd3',16.88,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G04','FAM','1246','Unkn','zd4',19.32,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G05','FAM','1246','Unkn','zd5',20.19,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G06','FAM','1246','Unkn','zd6',18.36,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G07','FAM','1246','Unkn','HSIL1',17.99,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G08','FAM','1246','Unkn','HSIL2',19.76,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G09','FAM','1246','Unkn','HSIL3',16.19,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G10','FAM','1246','Unkn','HSIL4',16.49,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G11','FAM','1246','Unkn','HSIL5',16.10,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('G12','FAM','1246','Unkn','HSIL6',12.93,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H01','FAM','1290','Unkn','zd1',27.73,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H02','FAM','1290','Unkn','zd2',25.66,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H03','FAM','1290','Unkn','zd3',23.87,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H04','FAM','1290','Unkn','zd4',26.15,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H05','FAM','1290','Unkn','zd5',27.95,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H06','FAM','1290','Unkn','zd6',26.42,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H07','FAM','1290','Unkn','HSIL1',23.70,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H08','FAM','1290','Unkn','HSIL2',25.06,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H09','FAM','1290','Unkn','HSIL3',24.57,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H10','FAM','1290','Unkn','HSIL4',22.65,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H11','FAM','1290','Unkn','HSIL5',30.81,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('H12','FAM','1290','Unkn','HSIL6',18.96,NULL,'K12');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A01','SYBR','b-actin','Unkn','zd1',30.40,NULL,'K1');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A02','SYBR','b-actin','Unkn','zd2',33.02,NULL,'K2');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A03','SYBR','b-actin','Unkn','zd3',32.52,NULL,'K3');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A04','SYBR','b-actin','Unkn','zd4',31.92,NULL,'K4');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A05','SYBR','b-actin','Unkn','zd5',32.18,NULL,'K5');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A06','SYBR','b-actin','Unkn','zd6',31.73,NULL,'K6');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A07','SYBR','b-actin','Unkn','HSIL1',23.03,NULL,'K7');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A08','SYBR','b-actin','Unkn','HSIL2',21.22,NULL,'K8');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A09','SYBR','b-actin','Unkn','HSIL3',26.50,NULL,'K9');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A10','SYBR','b-actin','Unkn','HSIL4',26.07,NULL,'K10');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A11','SYBR','b-actin','Unkn','HSIL5',32.84,NULL,'K11');
+INSERT INTO [InputData] ([A],[B],[C],[D],[E],[F],[G],[H]) 
+VALUES ('A12','SYBR','b-actin','Unkn','HSIL6',26.07,NULL,'K12');
